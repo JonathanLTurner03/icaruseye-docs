503237e6387f9afcbc0dcb54851d0fb860added7
Jonathan Turner <turner@atlantissrv.com>
2025-06-16 07:49:51 -0400

    Quartz sync: Jun 16, 2025, 7:49 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,8 +8,8 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "IcarusEye Docs",
-    pageTitleSuffix: "- IcarusEye Docs",
+    pageTitle: "IcarusEye Documentation",
+    pageTitleSuffix: " - IcarusEye",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
